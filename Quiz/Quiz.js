0364bdb9aafa5e524850a0233f3addbf99c3ac0c
YuliaM <yuliana.m@me.com>
2014-06-28 15:17:18 +0400

    dinamic radio buttuns added

--- Quiz/Quiz.js
+++ Quiz/Quiz.js
@@ -1,4 +1,3 @@
-
 var $ = function (id) {
     return document.getElementById(id);
 }
@@ -26,24 +25,45 @@ window.onload = function () {
 }
 
 var fillAnswers = function(){
-    var questionDiv = $('questionDiv');
-    var answerErrorDiv = $("answerError");
+    var questionDiv = $("questionDiv");
+    var answerErrorDiv = $("answerErrorDiv");
+    var nextButtonDiv = $("nextButtonDiv");
 
     if (questionNumber === allQuestions.length) {
+        questionDiv.innerHTML = "";
+        answerErrorDiv.innerHTML = "";
+        nextButtonDiv.innerHTML = "";
+
+
         var h2 = document.createElement('h2');
         h2.innerHTML = "Your total score is "+totalScore;
-        questionDiv.innerHTML = "";
-        questionDiv.appendChild(h2);
-        answerErrorDiv.innerHTML = "";
 
+        questionDiv.appendChild(h2);
 
     }
     else {
+        questionDiv.innerHTML = "";
         answerErrorDiv.innerHTML = "";
-        questionDiv.getElementsByTagName('h2').item(0).innerHTML = 'Question ' + (questionNumber + 1);
-        $("answer1").nextSibling.innerHTML = allQuestions[questionNumber].choices[0];
-        $("answer2").nextSibling.innerHTML = allQuestions[questionNumber].choices[1];
-        $("answer3").nextSibling.innerHTML = allQuestions[questionNumber].choices[2];
+
+        var h2 = document.createElement('h2');
+        h2.innerHTML = 'Question ' + (questionNumber + 1);
+        questionDiv.appendChild(h2);
+
+
+        for(var i in allQuestions[questionNumber].choices){
+            var label = document.createElement("label");
+            var radio = document.createElement("input");
+
+            radio.setAttribute("type", "radio");
+            radio.setAttribute("name", "answer_radio");
+            radio.setAttribute("value", allQuestions[questionNumber].choices[i]);
+
+            label.appendChild(radio);
+            label.innerHTML += allQuestions[questionNumber].choices[i]+"<br>";
+
+            questionDiv.appendChild(label);
+
+        }
     }
 }
 var nextQuestion = function(){
@@ -52,22 +72,22 @@ var nextQuestion = function(){
     if(checkedAnswer!=null) {
         var theQuestion = allQuestions[questionNumber];
         var correctAnswerText = theQuestion.choices[theQuestion.correctAnswer];
-        var checkedAnswerText = checkedAnswer.nextSibling.innerHTML;
 
-        if (checkedAnswerText === correctAnswerText) {
+        if (checkedAnswer.value === correctAnswerText) {
             totalScore++;
         }
         checkedAnswer.checked = false;
         questionNumber++;
+
         fillAnswers();
     }
     else{
         var h3 = document.createElement('h3');
         h3.innerHTML = "Select an answer";
-        answerError = $("answerError");
-        answerError.innerHTML = "";
-        answerError.appendChild(h3);
 
+        var answerErrorDiv = $("answerErrorDiv");
+        answerErrorDiv.innerHTML = "";
+        answerErrorDiv.appendChild(h3);
     }
 }
 function checkedRadioBtn(sGroupName)
